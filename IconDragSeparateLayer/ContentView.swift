adfd89e5fa551d06c178551bb0716190ae847797
Issac Penn <pbbsignup@outlook.com>
2019-08-25 12:57:58 +0800

    drag interaction complete

--- IconDragSeparateLayer/ContentView.swift
+++ IconDragSeparateLayer/ContentView.swift
@@ -9,8 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum DragState {
+        case inactive
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+    }
+    
+    @GestureState var dragState = DragState.inactive
+//    @State var viewState = CGSize.zero
+    
     var body: some View {
-        Text("Hello World")
+        
+        let dragGeture = DragGesture()
+            .updating($dragState) { (value, state, transaction) in
+                state = .dragging(translation: value.translation)
+        }
+        .onEnded { value in
+//            self.viewState = .zero
+        }
+        
+        var animationDuration: Double {
+            switch self.dragState {
+            case .inactive:
+                return 0.2
+            default:
+                return 0.02
+            }
+        }
+        
+        return ZStack {
+            Rectangle()
+                .frame(width: 75.0, height: 75.0, alignment: .center)
+                .foregroundColor(.yellow)
+                .cornerRadius(10, antialiased: true)
+                
+            Circle()
+                .frame(width: 30.0 , height: 30.0, alignment: .center)
+                .foregroundColor(.green)
+        }
+        .fixedSize()
+        .frame(alignment: .center)
+        .offset(dragState.translation)
+        .gesture(dragGeture)
+        .animation(.easeInOut(duration: animationDuration))
+        
+        
+        
     }
 }
 
